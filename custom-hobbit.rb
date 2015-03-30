c8ab778c62f6367744d674f9f4595d30dce23bcb
Erik Griffn <ErikAGriffin@users.noreply.github.com>
2015-03-30 17:36:45 +0100

    Update custom-hobbit.rb

--- custom-hobbit.rb
+++ custom-hobbit.rb
@@ -1,7 +1,7 @@
 module Hobbit
   module Hole
 
-# add any custom methods or pathing changes you would like to make here.
+# add any custom methods or pathing changes you would like to make here, then require this file.
 
   end
 end
