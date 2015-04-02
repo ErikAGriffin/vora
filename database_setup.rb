ccce0e03a7727055ac6f42b2dc5645701c34f0c1
Erik Griffin <griffin.erik@gmail.com>
2015-04-02 17:21:23 +0100

    Rearching Sequel

---
+++ database_setup.rb
@@ -0,0 +1,6 @@
+require 'sequel'
+
+env = ENV['VORA_DB_ENV'] || 'development'
+
+
+DB = Sequel.connect(ENV['DATABASE_URL'] || "postgres://localhost/vora_#{env}")
--- server.rb
+++ server.rb
@@ -1,21 +1,14 @@
 require 'hobbit'
 require 'hobbit/hole'
-require 'sequel'
 
 class Server < Hobbit::Base
   include Hobbit::Hole
 
-  env = ENV['VORA_DB_ENV'] || 'development'
-
-  DB = Sequel.connect(ENV['DATABASE_URL'] || "postgres://localhost/vora_#{env}")
 
 
-
-
-
-    get '/' do
-      render 'index'
-    end
+  get '/' do
+    render 'index'
+  end
 
 
 
