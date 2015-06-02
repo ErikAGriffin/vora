dc2cc5c55cc61931a876022e2e68bf17832329f7
Erik Griffin <griffin.erik@gmail.com>
2015-06-02 12:09:05 +0200

    Added some front-end frameworks

--- server.rb
+++ server.rb
@@ -1,19 +1,15 @@
 require 'hobbit'
 require 'hobbit/hole'
-require 'sequel'
 
 class Server < Hobbit::Base
   include Hobbit::Hole
 
-  env = ENV['VORA_DB_ENV'] || 'development'
-  DB = Sequel.connect(ENV['DATABASE_URL'] || "postgres://localhost/vora_#{env}")
+  require './database_setup'
 
-  DB.create_table?(:songs) do
-    primary_key :id
-    String :title
-  end
-
-  require './lib/model/song'
+  # DB.create_table?(:songs) do
+  #   primary_key :id
+  #   String :title
+  # end
 
 
   get '/' do
