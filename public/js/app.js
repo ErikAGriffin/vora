8561116791e1b70e73a664fdd8a7c70bb666ac2e
Erik Griffin <griffin.erik@gmail.com>
2015-06-02 21:24:58 +0200

    Barebones form

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,18 @@
-var app = angular.module('vora', ['ui.router','ngAnimate']);
+(function() {
+  var app = angular.module('vora', ['ui.router','ngAnimate']);
+
+  app.config(function($stateProvider,$urlRouterProvider,$locationProvider) {
+
+
+    $locationProvider.html5Mode(true);
+  });
+
+  app.controller('ApplicationController', function() {
+
+    var self = this;
+
+    
+
+  });
+
+}());
